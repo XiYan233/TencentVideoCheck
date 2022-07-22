30528cb5744bdb227d68592079786f93b554daa1
Demontisa <qq1994769112@gmail.com>
2022-07-22 17:48:37 +0800

    增加Cookie失效时发送通知

--- Server/Corn/CronTask.go
+++ Server/Corn/CronTask.go
@@ -64,6 +64,8 @@ func CronTask() {
 			if Refresh(cookie) {
 				log.Printf("Cookie未失效")
 			} else {
+				msg := "<font color=\\\"warning\\\">腾讯视频签到通知</font>\n" + "> Cookie状态已失效，请重新提交Cookie"
+				Notice(notice, noticeToken, msg)
 				log.Printf("Cookie已失效,正在删除Cookie")
 				deleteDB, err := db.Prepare("DELETE FROM `user` WHERE `Cookie`=?")
 				if err != nil {
